b78b5e8490536912e92567116215262d3ac58332
Richard Marko <rissko@gmail.com>
2011-10-21 17:03:10 +0200

    Morph to announce bot

--- bot.rb
+++ bot.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'open-uri'
-require 'nokogiri'
 
 require 'socket'
 require 'openssl'
@@ -14,6 +13,7 @@ USER = $cfg['irc']['user']
 CHAN = $cfg['irc']['chan']
 
 TIMEOUT = $cfg['timeout']
+MSG = $cfg['msg']
 
 class IRC
   def initialize(host, port, user, chan)
@@ -119,84 +119,12 @@ class IRC
   end
 end
 
-def scan(type, irc, say_to)
-  #type:
-  #  :changes
-  #  :most_recent
-  #  :all
-
-  site = $cfg['web']['site']
-  dw_recent = File.join(site, $cfg['web']['dw_recent'])
-
-  html = Nokogiri::HTML(open(dw_recent))
-  list = html.css('form#dw__recent > div > ul > li > div')
-
-  case type
-    when :changes
-      recent = open('recent', 'r')
-      r = recent.read.split "\n"
-
-      recent = open('recent', 'a')
-    when :most_recent
-      l = list[0]
-      list = []
-      list << l
-  end
-
-
-  list.each do |item|
-    begin
-      date = item.css('span.date').text.strip
-    rescue
-      date = nil
-    end
-    begin
-      href = item.css('a.wikilink1').attr('href').value.strip
-    rescue
-      href = nil
-    end
-    begin
-      sum = item.css('span.sum').text.strip
-    rescue
-      sum = nil
-    end
-    begin
-      user = item.css('span.user').text.strip
-    rescue
-      user = nil
-    end
-    formatted = "WEB NEWS: #{date} | #{user} | #{sum}\nurl: #{site}#{href}"
-
-    if (type == :changes) && (!r.include?(date.strip))
-      irc.say formatted, :to => say_to
-      recent.puts date
-    elsif [:most_recent, :all].include?(type)
-      irc.say formatted, :to => say_to
-    end
-  end
-
-  recent.close if type == :changes
-end
 
 irc = IRC.new(HOST, PORT, USER, CHAN)
 irc.connect()
 
 irc.on_timeout do
-  scan(:changes, irc, CHAN)
-end
-
-irc.command "mostrecent" do |chan, from|
-  to = chan
-  to = from if chan == USER
-
-  scan(:most_recent, irc, to)
-end
-
-irc.command "everything" do |chan, from|
-  to = chan
-  to = from if chan == USER
-
-  scan(:all, irc, to)
+  irc.say MSG, :to => CHAN
 end
 
 irc.command "ping" do |chan, from|
